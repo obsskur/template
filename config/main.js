2478407bbf378d92015468859ce4d2c8087d0e35
obsskur <makolise77@gmail.com>
2025-11-17 19:54:18 +0000

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 // This changes the title of your site
 
-var sitename = "Ultra Blue"; // Change this to change the name of your website.
+var sitename = "WinSploit"; // Change this to change the name of your website.
 var subtext = "v1.1"; // set the subtext
 
 // more settings in main.css
